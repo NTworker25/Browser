04671b57f1e01c9c63465ee2d721f0f225b78ccf
NTworker <mikky20101@gmail.com>
2025-10-12 20:15:37 +0300

    Toolbar styles and back buttons have been added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QMainWindow, QApplication, QToolBar, QAction, QLineEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QToolBar, QAction, QLineEdit, QToolButton
 from PyQt5.QtCore import QUrl
+from pygame.display import update
 
 
 class Browser(QMainWindow):
@@ -13,12 +14,50 @@ class Browser(QMainWindow):
         self.addToolBar(toolbar)
         back_btn = QAction("⬅️", self)
         back_btn.triggered.connect(self.browser.back)
+        toolbar.setStyleSheet("""
+        QToolBar {
+        background-color: #fff;
+        padding: 8px;
+        font-size: 14px;
+        color: #333;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+        text-align: center;
+        }
+        
+        QToolButton {
+        background-color: #222;
+        color: #fff;
+        padding: 6px 12px;
+        border: 1px solid #444;
+        border-radius: 4px;
+        min-width: 40px;
+        text-align: center; 
+        font-size: 20px;
+        }
+        
+        QToolButton:hover {
+        background-color: #333;
+        border-color: #888;
+        }
+        
+        QToolButton:pressed {
+        background-color: #444;
+        }
+        """)
         toolbar.addAction(back_btn)
 
         self.url_bar = QLineEdit()
+        self.url_bar.setStyleSheet("""
+        QLineEdit {
+        padding: 8px 16px;
+        font-size: 18px;
+        }
+        """)
         self.url_bar.returnPressed.connect(self.navigate_to_url)
         toolbar.addWidget(self.url_bar)
         self.setCentralWidget(self.browser)
+        self.browser.urlChanged.connect(self.update_url)
 
     def navigate_to_url(self):
         url_text = self.url_bar.text()
@@ -30,6 +69,9 @@ class Browser(QMainWindow):
             print(f"Ошибка: неверный URL {url_text}")
             return
         self.browser.setUrl(url)
+
+    def update_url(self, new_url):
+        self.url_bar.setText(new_url.toString())
 
 
 app = QApplication(sys.argv)
