c23ce44a04aab26e641c2e4508d2167fc100e0a4
NTworker <mikky20101@gmail.com>
2026-04-05 20:00:13 +0300

    Добавил виджет для вкладок и саму вкладку. Также закрытие вкладки и иконку брузера.

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ class Browser(QMainWindow):
     def __init__(self):
         super().__init__()
         self.full_url = ""
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://www.google.com"))
-        self.browser.urlChanged.connect(self.update_url_bar)
+        # self.browser = QWebEngineView()
+        # self.browser.setUrl(QUrl("https://www.google.com"))
+        # self.browser.urlChanged.connect(self.update_url_bar)
 
         # Настройки окна
         self.setGeometry(100, 100 , 1200, 800)
         self.setWindowTitle("RobustBrowser")
 
         # лейбл для кнопок навигации и поисковой строки
-        nav_bar = QWidget()
-        nav_bar.setFixedHeight(50)
-        nav_layout = QHBoxLayout(nav_bar)
+        # nav_bar = QWidget()
+        # nav_bar.setFixedHeight(50)
+        # nav_layout = QHBoxLayout(nav_bar)
 
         # Кнопки навигации
-        back_btn = QPushButton("←")
-        reset_btn = QPushButton("↻")
-        forward_btn = QPushButton("→")
+        # back_btn = QPushButton("←")
+        # reset_btn = QPushButton("↻")
+        # forward_btn = QPushButton("→")
 
         # Привязываем функции к кнопкам
-        back_btn.clicked.connect(self.browser.back)
-        reset_btn.clicked.connect(self.browser.reload)
-        forward_btn.clicked.connect(self.browser.forward)
+        # back_btn.clicked.connect(self.browser.back)
+        # reset_btn.clicked.connect(self.browser.reload)
+        # forward_btn.clicked.connect(self.browser.forward)
 
 
         # Создание поисковой строки и получение событий
@@ -45,24 +45,49 @@ class Browser(QMainWindow):
         self.url_bar.installEventFilter(self)
 
         # Добавляем кнопки в виджет
-        nav_layout.addWidget(back_btn)
-        nav_layout.addWidget(reset_btn)
-        nav_layout.addWidget(forward_btn)
-        nav_layout.addWidget(self.url_bar)
+        # nav_layout.addWidget(back_btn)
+        # nav_layout.addWidget(reset_btn)
+        # nav_layout.addWidget(forward_btn)
+        # nav_layout.addWidget(self.url_bar)
 
+        tab_container = QHBoxLayout()
+        self.tabs = QTabWidget()
+        self.tabs.tabCloseRequested.connect(self.close_tab)
+        self.tabs.setTabsClosable(True)
+        self.setCentralWidget(self.tabs)
+
+        new_tab_button = QPushButton("+")
+        new_tab_button.setFixedSize(30, 30)
+        tab_container.addWidget(new_tab_button)
         # Создание окна
-        central_widget = QWidget()
-        central_layout = QVBoxLayout(central_widget)
-        central_layout.setContentsMargins(0, 0, 0, 0)
-        central_layout.setSpacing(0)
-        central_layout.addWidget(nav_bar, 0)  # Панель навигации с stretch=0 (не растягивается)
-        central_layout.addWidget(self.browser, 1)  # Браузер с stretch=1 (занимает всё оставшееся пространство)
-        # central_layout.addWidget(self.tab, 1)
-
-        self.setCentralWidget(central_widget)
+        # central_widget = QWidget()
+        # central_layout = QVBoxLayout(central_widget)
+        # central_layout.setContentsMargins(0, 0, 0, 0)
+        # central_layout.setSpacing(0)
+        # central_layout.addWidget(nav_bar, 0)  # Панель навигации с stretch=0 (не растягивается)
+        # central_layout.addWidget(self.browser, 1)  # Браузер с stretch=1 (занимает всё оставшееся пространство)
+        # # central_layout.addWidget(self.tab, 1)
+        #
+        # self.setCentralWidget(central_widget)
 
         # Виждет вкладок
-        tab_widgets = QTabWidget()
+        # tab_widgets = QTabWidget()
+        self.add_new_tab()
+
+    def add_new_tab(self):
+        tab = TabWidget()
+        index = self.tabs.addTab(tab, "Новая вкладка")
+        self.tabs.setCurrentIndex(index)
+        # if qurl is None:
+        #     qurl = QUrl("https://www.google.com")
+        # self.web_engine = QWebEngineView()
+        # self.web_engine.setUrl(qurl)
+
+    def close_tab(self, tab_index):
+        if self.tabs.count() == 1:
+            self.close()
+        else:
+            self.tabs.removeTab(index=tab_index)
 
     def validate_url(self) -> QUrl | None:
         full_url_text = self.url_bar.text()
@@ -116,7 +141,8 @@ class Browser(QMainWindow):
                     self.url_bar.setText(short_url)
         return super().eventFilter(obj, event)
 
-app = QApplication(sys.argv)
-main_window = Browser()
-main_window.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_window = Browser()
+    main_window.show()
+    sys.exit(app.exec_())
--- tab.py
+++ tab.py
@@ -1,18 +1,17 @@
-from PyQt5.QtWidgets import QTabWidget
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
-class TabWidget(QTabWidget):
+class TabWidget(QWidget):
     def __init__(self):
         super().__init__()
-        self.setTabsClosable(True)
-        self.tabBar().setExpanding(True)
+        # self.tabBar().setExpanding(True)
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://www.google.com"))
+        # self.browser.urlChanged.connect(self.update_url_bar)
 
-    def add_new_tab(self, qurl=None, label='Blank'):
-        if qurl is None:
-            qurl = QUrl("https://www.google.com")
-        self.web_engine = QWebEngineView()
-        self.web_engine.setUrl(qurl)
-
+        layout = QVBoxLayout()
+        layout.addWidget(self.browser)
+        self.setLayout(layout)
 
 
