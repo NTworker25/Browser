a029af1ccd61eb89188885710f9d61b28449396b
NTworker <mikky20101@gmail.com>
2026-04-16 22:04:07 +0300

    Провёл крупный рефакторинг системы вкладок и кода в целом.

--- main.py
+++ main.py
@@ -13,52 +13,27 @@ from tab import TabWidget
 class Browser(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.full_url = ""
-        # self.browser = QWebEngineView()
-        # self.browser.setUrl(QUrl("https://www.google.com"))
-        # self.browser.urlChanged.connect(self.update_url_bar)
 
         # Настройки окна
-        self.setGeometry(100, 100 , 1200, 800)
+        self.setGeometry(100, 100, 1200, 800)
         self.setWindowTitle("RobustBrowser")
 
-        # лейбл для кнопок навигации и поисковой строки
-        # nav_bar = QWidget()
-        # nav_bar.setFixedHeight(50)
-        # nav_layout = QHBoxLayout(nav_bar)
+        # tab_container = QWidget()
+        # tabs_layout = QHBoxLayout(tab_container)
+        # tabs_layout.setContentsMargins(0, 0, 0, 0)
+        # tabs_layout.setSpacing(0)
 
-        # Кнопки навигации
-        # back_btn = QPushButton("←")
-        # reset_btn = QPushButton("↻")
-        # forward_btn = QPushButton("→")
+        # new_tab_button = QPushButton("+")
+        # new_tab_button.setFixedSize(30, 30)
 
-        # Привязываем функции к кнопкам
-        # back_btn.clicked.connect(self.browser.back)
-        # reset_btn.clicked.connect(self.browser.reload)
-        # forward_btn.clicked.connect(self.browser.forward)
-
-
-        # Создание поисковой строки и получение событий
-        self.url_bar = QLineEdit()
-        self.url_bar.setStyleSheet(URL_BAR_STYLE)
-        self.url_bar.returnPressed.connect(self.validate_url)
-        self.url_bar.installEventFilter(self)
-
-        # Добавляем кнопки в виджет
-        # nav_layout.addWidget(back_btn)
-        # nav_layout.addWidget(reset_btn)
-        # nav_layout.addWidget(forward_btn)
-        # nav_layout.addWidget(self.url_bar)
-
-        tab_container = QHBoxLayout()
         self.tabs = QTabWidget()
         self.tabs.tabCloseRequested.connect(self.close_tab)
         self.tabs.setTabsClosable(True)
         self.setCentralWidget(self.tabs)
 
-        new_tab_button = QPushButton("+")
-        new_tab_button.setFixedSize(30, 30)
-        tab_container.addWidget(new_tab_button)
+        # tabs_layout.addWidget(self.tabs, 1)
+        # tabs_layout.addWidget(new_tab_button)
+
         # Создание окна
         # central_widget = QWidget()
         # central_layout = QVBoxLayout(central_widget)
@@ -66,8 +41,8 @@ class Browser(QMainWindow):
         # central_layout.setSpacing(0)
         # central_layout.addWidget(nav_bar, 0)  # Панель навигации с stretch=0 (не растягивается)
         # central_layout.addWidget(self.browser, 1)  # Браузер с stretch=1 (занимает всё оставшееся пространство)
-        # # central_layout.addWidget(self.tab, 1)
-        #
+        # central_layout.addWidget(self.tab, 1)
+
         # self.setCentralWidget(central_widget)
 
         # Виждет вкладок
@@ -89,57 +64,6 @@ class Browser(QMainWindow):
         else:
             self.tabs.removeTab(index=tab_index)
 
-    def validate_url(self) -> QUrl | None:
-        full_url_text = self.url_bar.text()
-        if not full_url_text.startswith("http"):
-            full_url_text = "https://" + full_url_text
-        url = QUrl(full_url_text)
-        if not url.isValid():
-            print(f'Ошибка: Неверный URL {url.toString()}')
-            return None
-        return url
-
-    def go_to_url(self):
-        url = self.validate_url()
-        if url is None:
-            return
-        self.browser.setUrl(url)
-
-    def cut_url(self, url_text: str) -> str:
-        qurl = QUrl(url_text)
-        if not qurl.isValid():
-            return url_text
-        host = qurl.host()
-        if not host:
-            return url_text
-        parts = host.split('.')
-        if len(parts) < 2:
-            return host
-        domain = '.'.join(parts[-2:])
-        if len(parts) > 2:
-            subdomain = '.'.join(parts[:-2])
-            return f"{subdomain}.{domain}"
-        else:
-            return domain
-
-    def update_url_bar(self, new_url: QUrl) -> None:
-        self.full_url = new_url.toString()
-        if not self.url_bar.hasFocus():
-            short_url = self.cut_url(self.full_url)
-            self.url_bar.setText(short_url)
-        else:
-            self.url_bar.setText(self.full_url)
-
-    def eventFilter(self, obj, event):
-        if obj == self.url_bar:
-            if event.type() == QEvent.FocusIn:
-                if self.full_url:
-                    self.url_bar.setText(self.full_url)
-            elif event.type() == QEvent.FocusOut:
-                if self.full_url:
-                    short_url = self.cut_url(self.full_url)
-                    self.url_bar.setText(short_url)
-        return super().eventFilter(obj, event)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- tab.py
+++ tab.py
@@ -1,6 +1,8 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
-from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit
+from PyQt5.QtCore import QUrl, QEvent
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from styles import URL_BAR_STYLE
+
 
 class TabWidget(QWidget):
     def __init__(self):
@@ -8,10 +10,100 @@ class TabWidget(QWidget):
         # self.tabBar().setExpanding(True)
         self.browser = QWebEngineView()
         self.browser.setUrl(QUrl("https://www.google.com"))
-        # self.browser.urlChanged.connect(self.update_url_bar)
+        self.full_url = ""
+        self.browser.urlChanged.connect(self.update_url_bar)
 
+        # Создание поисковой строки и получение событий
+        self.url_bar = QLineEdit()
+        self.url_bar.setStyleSheet(URL_BAR_STYLE)
+        self.url_bar.returnPressed.connect(self.validate_url)
+        self.url_bar.installEventFilter(self)
+
+        # Лейбл для кнопок навигации и поисковой строки
+        nav_bar = QWidget()
+        nav_bar.setFixedHeight(50)
+        nav_layout = QHBoxLayout(nav_bar)
+
+        # Кнопки навигации
+        back_btn = QPushButton("←")
+        reset_btn = QPushButton("↻")
+        forward_btn = QPushButton("→")
+
+        # Привязываем функции к кнопкам
+        back_btn.clicked.connect(self.browser.back)
+        reset_btn.clicked.connect(self.browser.reload)
+        forward_btn.clicked.connect(self.browser.forward)
+
+        # Добавляем кнопки в виджет
+        nav_layout.addWidget(back_btn)
+        nav_layout.addWidget(reset_btn)
+        nav_layout.addWidget(forward_btn)
+        nav_layout.addWidget(self.url_bar)
+
+        # Создание главного контейнера
         layout = QVBoxLayout()
+        layout.addWidget(nav_bar)
         layout.addWidget(self.browser)
         self.setLayout(layout)
 
+    def validate_url(self) -> QUrl | None:
+        """Валидация URL"""
 
+        full_url_text = self.url_bar.text()
+        if not full_url_text.startswith("http"):
+            full_url_text = "https://" + full_url_text
+        url = QUrl(full_url_text)
+        if not url.isValid():
+            print(f'Ошибка: Неверный URL {url.toString()}')
+            return None
+        return url
+
+    def go_to_url(self):
+        """Переход по текущей ссылке"""
+
+        url = self.validate_url()
+        if url is None:
+            return
+        self.browser.setUrl(url)
+
+    def cut_url(self, url_text: str) -> str:
+        """Обрезка URL"""
+
+        qurl = QUrl(url_text)
+        if not qurl.isValid():
+            return url_text
+        host = qurl.host()
+        if not host:
+            return url_text
+        parts = host.split('.')
+        if len(parts) < 2:
+            return host
+        domain = '.'.join(parts[-2:])
+        if len(parts) > 2:
+            subdomain = '.'.join(parts[:-2])
+            return f"{subdomain}.{domain}"
+        else:
+            return domain
+
+    def update_url_bar(self, new_url: QUrl) -> None:
+        """Записывает ссылку в интерфейс, отображение ссылки"""
+
+        self.full_url = new_url.toString()
+        if not self.url_bar.hasFocus():
+            short_url = self.cut_url(self.full_url)
+            self.url_bar.setText(short_url)
+        else:
+            self.url_bar.setText(self.full_url)
+
+    def eventFilter(self, obj, event):
+        """Перехватывает события url_bar и выполняет нужные действия"""
+
+        if obj == self.url_bar:
+            if event.type() == QEvent.FocusIn:
+                if self.full_url:
+                    self.url_bar.setText(self.full_url)
+            elif event.type() == QEvent.FocusOut:
+                if self.full_url:
+                    short_url = self.cut_url(self.full_url)
+                    self.url_bar.setText(short_url)
+        return super().eventFilter(obj, event)
